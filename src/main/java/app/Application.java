7872b29d1cb09fcc599c5a17786e16b170603159
Philipp Zeppelin <philippzeppelin>
2024-03-20 23:43:14 +0300

    Finished task 2.1.3

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -6,15 +6,12 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Application {
-
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
         for (int i = 0; i < 5; i++) {
-            AnimalsCage bean =
-                    applicationContext.getBean(AnimalsCage.class);
+            AnimalsCage bean = applicationContext.getBean(AnimalsCage.class);
             bean.whatAnimalSay();
         }
     }
-
 }
