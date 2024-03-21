8a31b9e1aa6c30deb3ad8277ca87a2cd97bfe1da
Philipp Zeppelin <philippzeppelin>
2024-03-21 20:25:12 +0300

    Fixed a code: - In AnimalsCage class set fields to constructor - Made Dog bean without Qualifier

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -1,21 +1,26 @@
 package app.model;
 
+import app.config.AppConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AnimalsCage {
-    @Qualifier("dog")
-    @Autowired
-    private Animal animal;
+    private final Animal dog;
+
+    private final Timer timer;
 
     @Autowired
-    private Timer timer;
+    public AnimalsCage(Animal dog, Timer timer) {
+        this.dog = dog;
+        this.timer = timer;
+    }
 
     public void whatAnimalSay() {
         System.out.println("Say:");
-        System.out.println(animal.toString());
+        System.out.println(dog.toString());
         System.out.println("At:");
         System.out.println(timer.getTime());
         System.out.println("________________________");
--- src/main/java/app/model/Dog.java
+++ src/main/java/app/model/Dog.java
@@ -1,5 +1,6 @@
 package app.model;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
